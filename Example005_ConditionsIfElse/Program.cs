647a623f261ea268730bb38ec4f9f5f7967946a3
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-01-26 21:56:14 +0500

    add example if-else

---
+++ Example005_ConditionsIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+string userNameOrigin = userName; 
+if (userName.ToLower() == "алена")
+{
+    Console.WriteLine("Ура! Это же " + userNameOrigin + "!");
+}
+else
+{
+    Console.WriteLine("Привет " + userName);
+}
+
