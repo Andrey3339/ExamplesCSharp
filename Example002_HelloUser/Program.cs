beba30ff1aead277273b2ed7b11243e6531fbc3e
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-01-26 20:21:47 +0500

    Example 002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите ваше имя: ");
+string userName = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(userName);
+Console.WriteLine(". Ну где ты был?, ну обними меня скорей!");
+
+
+
+
