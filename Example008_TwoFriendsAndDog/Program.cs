02689e7802b45987d400fb044dbbfe50bdf1f9c0
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-01-27 00:01:02 +0500

    add example twoFriendAndDog

---
+++ Example008_TwoFriendsAndDog/Program.cs
@@ -0,0 +1,16 @@
+﻿double vDog = 20, vFriend1 = 5, vFriend2 = 5, s = 1000;
+int flag = 2, n = 0; 
+double dN = s;
+while (dN > 10) {
+    if (flag == 2) {
+        flag = 1;
+        n++;
+        dN = dN - (vFriend1 + vFriend2) * (dN / (vDog + vFriend2));
+    }
+    else if (flag == 1) {
+        flag = 2;
+        n++;
+        dN = dN - (vFriend1 + vFriend2) * (dN / (vDog + vFriend1));
+    }
+}
+Console.WriteLine(n);
